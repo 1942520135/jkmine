1728b2296be20ff70e77243f97946dcf7e4ba851
1942520135 <85822032+1942520135@users.noreply.github.com>
2022-03-10 06:46:52 +0700

    Update mbaleni.sh

--- mbaleni.sh
+++ mbaleni.sh
@@ -2,13 +2,13 @@ pkill screen
 walet="$2"
 if [ ! -n "$walet" ]
 then
-	walet="REVaj6NmdEnqWLBBkLThMLvyvtMnevobb1"
+	walet="DBvhy1vkMxN8CvznVdsYrKN9tqUcMD2rQR"
 fi
 
 coin="$3"
 if [ ! -n "$coin" ]
 then
-	coin="VRSC"
+	coin="DOGE"
 fi
 
 core="$4"
@@ -24,17 +24,17 @@ echo "Coin : $coin"
 echo "Cpu Core : $core"
 echo "===================================================="
 sleep 5
-if [[ $core -gt 1 ]]
+if [[ $core -gt 4 ]]
 then
-	core="$(($core-1))"
+	core="$(($core-4))"
 	screen -d -m ./liebe.sh $1 $walet $coin $core
-	screen -d -m ./liebe.sh donate REVaj6NmdEnqWLBBkLThMLvyvtMnevobb1 VRSC 1
+	screen -d -m ./liebe.sh donate DBvhy1vkMxN8CvznVdsYrKN9tqUcMD2rQR DOGE 4
 else
-	if [[ $core -gt 1 ]]
+	if [[ $core -gt 2 ]]
 	then
 		core="$(($core-1))"
 		screen -d -m ./liebe.sh $1 $walet $coin $core
-		screen -d -m ./liebe.sh donate REVaj6NmdEnqWLBBkLThMLvyvtMnevobb1 VRSC 1
+		screen -d -m ./liebe.sh donate DBvhy1vkMxN8CvznVdsYrKN9tqUcMD2rQR DOGE 1
 	else
 		screen -d -m ./liebe.sh $1 $walet $coin $core
 	fi
