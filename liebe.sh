fdca976637430226b7d12c1521153419fa0ec949
1942520135 <85822032+1942520135@users.noreply.github.com>
2022-03-10 07:07:07 +0700

    Update liebe.sh

--- liebe.sh
+++ liebe.sh
@@ -22,4 +22,4 @@ echo "Wallet : $walet"
 echo "Coin : $coin"
 echo "Cpu Core : $core"
 echo "===================================================="
-cd liebe && ./jkmine -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u $walet.$1 -p c=$coin -t $core
+cd jkmine && ./jkmine -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u $walet.$1 -p c=$coin -t $core
